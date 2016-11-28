45dfec8e0429e065e19003292c9c74357f00778c
AjitHegde <ajithegde121@gmail.com>
2016-11-28 23:16:00 +0530

    Completed Mini Project
    Added all functionalities and finished the project

--- Showroom.cpp
+++ Showroom.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
+#include<stdlib.h>
 #include<fstream>
 #include "Car_showroom.cpp"
+#include<limits>
+#include<windows.h>
 using namespace std;
 
 
@@ -17,7 +20,11 @@ public:
     void readFromFile();
     void deleteCar();
     char* getName();
+    char* getColour();
+    double getPrice();
     void updateCar();
+    void searchCar();
+    friend void SetColor(int ForgC);
 };
 ShowRoom cars;
 int cnt=0;
@@ -26,6 +33,28 @@ char* ShowRoom::getName()
 {
     return carName;
 }
+char* ShowRoom::getColour()
+{
+    return carColour;
+}
+double ShowRoom::getPrice()
+{
+    return carPrice;
+}
+void SetColor(int ForgC)
+ {
+        WORD wColor;
+
+        HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+        if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
+        {
+            wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
+            SetConsoleTextAttribute(hStdOut, wColor);
+        }
+        return;
+ }
 void ShowRoom::writeToFile()
 {
     ofstream car("Car.txt",ios::binary|ios::app);
@@ -34,6 +63,8 @@ void ShowRoom::writeToFile()
     cin>>cars.addedDate;
     cin.ignore();
     car.write(reinterpret_cast<char*>(&cars),sizeof(ShowRoom));
+    system("cls");
+    cout<<"\n\n*** Car added ***\n\n";
     car.close();
     cnt++;
 }
@@ -92,6 +123,7 @@ void ShowRoom::updateCar()
 				fp.write(reinterpret_cast<char*>(&m1),sizeof(ShowRoom));
 				flag=1;
 				cout<<"\n*** Details Updated ***\n\n";
+				return;
 			}
 		}
 	}
@@ -139,11 +171,70 @@ void ShowRoom::deleteCar()
 	rename("new.txt", "Car.txt");
 }
 
+void ShowRoom::searchCar()
+{
+    int opt;
+    char n[100];
+    double cost;
+	fstream fp;
+	int flag=0;
+	ShowRoom m1;
+	cin.ignore();
+	cout<<"Search car by \n1->Name \t 2->Colour \t 3->Price \t 4->Go Back\n: ";
+	cin>>opt;
+	fp.open("Car.txt",ios::in| ios::out|ios::binary);
+	if(!fp)
+	{
+		cout<<"*** File not Found ***";
+		return;
+	}
+	else
+	{
+		    switch(opt)
+            {
+                    case 1: cout<<"Enter Car name: "; cin>>n;
+                            while(!fp.eof())
+                            {
+                                fp.read(reinterpret_cast<char*>(&m1),sizeof(ShowRoom));
+                                if(!fp.eof())
+                                    if(strcmp(n,m1.getName())==0)
+                                        {m1.showCarInfo();cout<<"\n\n";flag=1;}
+                            }break;
+                    case 2: cout<<"Enter car colour: "; cin>>n;
+                            while(!fp.eof())
+                            {
+                                fp.read(reinterpret_cast<char*>(&m1),sizeof(ShowRoom));
+                                if(!fp.eof())
+                                    if(strcmp(n,m1.getColour())==0)
+                                        {m1.showCarInfo();cout<<"\n\n";flag=1;}
+                            };break;
+                    case 3: cout<<"Enter car price: "; cin>>cost;
+                            while(!fp.eof())
+                            {
+                                fp.read(reinterpret_cast<char*>(&m1),sizeof(ShowRoom));
+                                if(!fp.eof())
+                                    if(cost>=m1.getPrice())
+                                         {m1.showCarInfo();cout<<"\n\n";flag=1;}
+                            }break;
+                    case 4: return;
+                        default: cout<<"\n\n*** Invalid option ***\n\n";
+                                return;
+			}
+		}
+    if(flag==0)
+        cout<<"\n\n*** No cars found for your search :-( ***\n\n";
+	fp.close();
+}
 int main()
 {
     ShowRoom m;
     int opt=0;
-    do
+    cout.setf(ios::fixed);
+    cout.precision(2);
+    SetColor(11);
+    cout<<"PCP PROJECT BY AJIT HEGDE(16BM1IS400) AND PRAJWAL B.S.(1BM15IS015)\n\n";
+    SetColor(15);
+    while(1)
     {
         cout<<"1->Add new car.\n";
         cout<<"2->Remove car.\n";
@@ -151,7 +242,11 @@ int main()
         cout<<"4->Display cars in showroom.\n";
         cout<<"5->Search for car.\n";
         cout<<"6->Exit.\n:";
-        cin>>opt;
+        while(!(cin>>opt)) {
+            cout << "Incorrect input. Please try again.\n";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
         switch(opt)
         {
         case 1:
@@ -168,9 +263,14 @@ int main()
         case 2: m.deleteCar(); break;
         case 3: m.updateCar(); break;
         case 4: m.readFromFile(); break;
-        //  case 5:
+        case 5: m.searchCar(); break;
+        case 6: return 0;
+        default:
+                cout<<"\n\n**** Invalid option ****\n\n";
+                break;
         }
     }
-    while(opt!=6);
     return 0;
 }
+
+
